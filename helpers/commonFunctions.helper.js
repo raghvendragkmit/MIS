e3ecd346c4147e55a425a223373550f92dcf1ab1
raghvendragkmit <raghvendra@gkmit.co>
2023-02-14 22:26:56 +0530

    fix: rename files

--- helpers/commonFunctions.helper.js
+++ helpers/commonFunctions.helper.js
@@ -1,4 +1,4 @@
-const { commonErrorHandler } = require('./errorHandler');
+const { commonErrorHandler } = require('./errorHandler.helper');
 
 const validateRequest = (req, res, next, schema, requestParamterType) => {
   let requestData = {};
