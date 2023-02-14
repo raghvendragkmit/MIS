a48224ff540f0ca33eea31809306a0a7ad0c306e
raghvendragkmit <raghvendra@gkmit.co>
2023-02-14 21:49:35 +0530

    feat: add initial server

---
+++ helpers/errorHandler.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const commonErrorHandler = async (req, res, message, statusCode = 500, error = null) => {
+  if (req.files) {
+    Object.keys(req.files).forEach(file => {
+      if (req.files[file].path) {
+        fs.unlink(req.files[file].path, err => {
+          console.log(err);
+        });
+      }
+    });
+  }
+  let errorMessage = 'Something went wrong. Please try again';
+  if (message) {
+    errorMessage = message;
+  }
+
+  if (error && error.message) {
+    errorMessage = error.message;
+  }
+  req.error = error;
+
+  const response = {
+    statusCode,
+    data: {},
+    message: errorMessage
+  };
+
+  res.status(statusCode).json(response);
+};
+
+module.exports = {
+  commonErrorHandler
+};
